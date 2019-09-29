e2fa8e2ced60e21dbaf39b402669a583a04ed516
ManuelSerna <manuelserna1997@gmail.com>
2019-09-29 15:45:53 -0500

    Completed pre-processing task.

--- hw-2/preprocess.py
+++ hw-2/preprocess.py
@@ -12,13 +12,60 @@ Read each line and extract a 10-word window around the first instance of a targe
 '''
 #************************************************
 
+import sys
 import string
 
-# Take in each line from file as its own element in the list
-raw_lines = [line.rstrip('\n') for line in open("amazon_reviews.txt")]
+#================================================
+# Disambiguate word senses using word pairs (word1, word2)
+# Return probabilities for each word in the pair.
+#================================================
+def wsd(word1, word2):
+    # Preprocess text--remove punctuation, 
+    file_name = 'amazon_reviews.txt'
+    text = open(file_name, 'r')
+    raw_text = text.read()
+    #formatted_text = raw_text.lower().replace('&quot', '').translate(str.maketrans('', '', string.punctuation)).translate(str.maketrans('', '', string.digits))
+    formatted_text = raw_text.lower().replace('&quot', '').translate(str.maketrans('', '', string.punctuation))
+    words = formatted_text.split() # list of all words from text
+    
+    # Create dictionary to hold context windows for each word in the pair.
+    # These context windows are entire strings.
+    instances = {}
+    instances[word1] = []
+    instances[word2] = []
+    
+    # Get total counts for the individual words
+    word_counts = {}
+    word_counts[word1] = 0
+    word_counts[word2] = 0
+    
+    # Create pseudoword
+    pseudoword = word1+word2
 
-# Lower-case everything
-for i in range(len(raw_lines)):
-    #raw_lines[i] = raw_lines[i].replace('&quot', '').translate(str.maketrans('', '', string.punctuation)).lower()
-    raw_lines[i] = raw_lines[i].replace('&quot', '').lower()
+    # Get context windows (+/- 10 words) if any words in a word pair are found.
+    for i in range(len(words)-1):
+        if words[i] == word1:
+            instance = words[i-10:i+10]
+            
+            # Replace word with pseudoword (og word remains as key in dict)
+            instance[10] = pseudoword
+            instances[word1].append(' '.join(instance))
+            
+            # Increase count
+            word_counts[word1] += 1
+        
+        if words[i] == word2:
+            instance = words[i-10:i+10]
+            
+            # Replace word with pseudoword (og word remains as key in dict)
+            instance[10] = pseudoword
+            instances[word2].append(' '.join(instance))
+            
+            # Increase count
+            word_counts[word2] += 1
+    
+    print(word_counts)
 
+
+# Call method and take in words as arguments
+wsd(sys.argv[1], sys.argv[2])
--- hw-2/run.sh
+++ hw-2/run.sh
@@ -3,6 +3,8 @@
 set -u
 set -e
 
-python preprocess.py
+python preprocess.py night seat
+python preprocess.py kitchen cough
+python preprocess.py car bike
 
 echo "Done."
