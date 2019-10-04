c33aed2e1dd5b4ed923c205fdb2dfbc972d7464f
ManuelSerna <manuelserna1997@gmail.com>
2019-10-03 19:01:15 -0500

    Redoing probability calculation strategy

--- hw-2/wsd.py
+++ hw-2/wsd.py
@@ -18,99 +18,152 @@ import json
 
 #================================================
 # Disambiguate word senses using word pairs (word1, word2)
-# Return probabilities for each word in the pair.
+# Return probabilities for each word "sense" for some pseudoword=word1+word2
 #================================================
 def wsd(word1, word2):
-    # Preprocess text--remove punctuation, 
+    # Preprocess text--remove punctuation, html residue, and digits
     file_name = 'amazon_reviews.txt'
     text = open(file_name, 'r')
     raw_text = text.read()
     text.close()
-    #formatted_text = raw_text.lower().replace('&quot', '').translate(str.maketrans('', '', string.punctuation)).translate(str.maketrans('', '', string.digits))
-    formatted_text = raw_text.lower().replace('&quot', '').translate(str.maketrans('', '', string.punctuation))
-    words = formatted_text.split() # list of all words from text
+    formatted_text = raw_text.lower().replace('&quot', '').translate(str.maketrans('', '', string.punctuation)).translate(str.maketrans('', '', string.digits))
     
     '''
-    Create dictionary to hold context windows for each word in the pair.
-        - These context windows are entire strings.
+    Variables
     '''
+    words          = formatted_text.split() # all words from text file
+    pseudoword     = word1+word2 # pseudoword that could be any of two senses
+    r              = 10 # range r for all context windows
+    windows        = {} # context windows for each "sense"
+    windows[word1] = []
+    windows[word2] = []
+    n_1            = 0 # total count of all words under "sense" 1
+    n_2            = 0 # total count of all words under "sense" 2
+    num_vocab      = 0 # number of unique words
+    uniquew        = {} # helps count unique words (for num_vocab)
+    
+    '''
+    Generate data
+    '''
+    for w in range(len(words)-1):
+        # Get context windows for each sense
+        if words[w] == word1:
+            c = words[w-r:w+r]
+            c[10] = pseudoword
+            windows[word1].append(c)
+        if words[w] == word2:
+            c = words[w-r:w+r]
+            c[10] = pseudoword
+            windows[word2].append(c)
+        # Increase num_vocab is word not encountered before
+        if words[w] not in uniquew:
+            uniquew[words[w]] = words[w]
+            num_vocab += 1
+    
+    #    #    #print(words[w])
+    #    #    uniquew.append(words[w])
+    #    #print(words[w])
+    #print(uniquew)
+    #with open("uniquew.json", 'w') as file:
+    #    file.write(json.dumps(uniquew, indent=4))
+    #print(num_vocab)
+    '''
+    For each word "sense", 
+    '''
+    #for w in 
+    
+    '''
+    #Create dictionary to hold context windows for each word in the pair.
+    #Write to a file so we can check our work.
     c_windows = {}
     c_windows[word1] = []
     c_windows[word2] = []
     
-    '''
-    Store counts for each word appearing in each context window from the above list. 
-    Indices under each key will correspond to the same context window.
-    '''
-    c_word_counts = {}
-    c_word_counts[word1] = []
-    c_word_counts[word2] = []
+    #Dictionary that stores frequencies of each "sense" and pseudoword.
+    pseudoword = word1+word2
     
-    '''
-    Dictionaries that store frequencies of each "sense"
-    '''
     senses = {}
     senses[word1] = 0
     senses[word2] = 0
-    
-    # Create pseudoword
-    pseudoword = word1+word2
+    senses[pseudoword] = 0
 
-    '''
-    Get context windows (+/- 10 words) if any words in a word pair are found.
-    '''
+    
+    #Generate list of documents "documents"
+    #    - Store each unique word with its count for each context window c of +/- 10 words. Sense is key.
+    #    Note: word sense will always be at index i=10.
+    
+    #Define num_vocab, counts number of unique words across all context windows.
+    
+    documents = []
+    num_vocab = 0
+    
     for i in range(len(words)-1):
         if words[i] == word1:
-            # Get context window c
             c = words[i-10:i+10]
             
-            # Count how many times each word in each context window appears
-            
-            '''
-            counts = {}
+            # Count unique words
+            d = {}
             for i in range(len(c)):
-                if c[i] not in counts:
-                    counts[i] = 1
+                if c[i] not in d:
+                    d[c[i]] = 1
                 else:
-                    counts[i] += 1
-            '''
+                    d[c[i]] += 1
+            documents.append(d)
             
-            # Replace word with pseudoword (og word remains as key in dict)
-            c[10] = pseudoword
+            # Create sentence string to later store in file
             c = ' '.join(c)
             c_windows[word1].append(c)
             
-            # Increase count
+            # Add to counts
             senses[word1] += 1
+            senses[pseudoword] += 1
         
+        # Apply same logic to sense 2
         if words[i] == word2:
-            # Get context window c
             c = words[i-10:i+10]
-            
-            # Count how many times each word in c appears
-            '''
-            counts = {}
+            d = {}
             for i in range(len(c)):
-                if c[i] not in counts:
-                    counts[i] = 1
+                if c[i] not in d:
+                    d[c[i]] = 1
                 else:
-                    counts[i] += 1
-            '''
-            
-            # Replace word with pseudoword (og word remains as key in dict)
-            c[10] = pseudoword
+                    d[c[i]] += 1
+            documents.append(d)
             c = ' '.join(c)
             c_windows[word2].append(c)
-            
-            # Increase count
             senses[word2] += 1
+            senses[pseudoword] += 1
     
-    # output context windows to better visualize data
-    with open("c_windows.json", 'w') as file:
-        file.write(json.dumps(c_windows, indent=4))
-    # output context window word counts dict
-    with open("c_word_counts.json", 'w') as file:
-        file.write(json.dumps(c_word_counts, indent=4))
+    
+    #Generate probabilities dictionary "probs" on 80% of the 
+    #    - For each unique word, calculate probability of it appearing given one of the two sense. Sense is key, value is P(word(i) | sense).
+    
+    probs = []
+    
+    '''
+    
+    
+    #for sense in c_windows:
+    
+    #print(senses)
+    
+    #Calculate the probabilities for each unique word in the documents
+    
+    #P(word_k | sense) = (n_k + 1)/(total_count_in_class + distinct_words) 
+    #where
+    
+    #n_k                  := frequency of word across all documents under a certain sense
+    #total_count_in_class := total counts of tat unique word
+    #distinct_words       := number of all distinct words across all documents
+    
+    
+    
+    #with open("c_windows.json", 'w') as file:
+    #    file.write(json.dumps(c_windows, indent=4))
+    #with open("documents.json", 'w') as file:
+    #    file.write(json.dumps(documents, indent=4))
+    
+    with open("windows.json", 'w') as file:
+        file.write(json.dumps(windows, indent=4))
     
 
 # Call method and take in words as arguments
